a2fd6638717e1a668c608e4ed737c48da972f33b
Takeshi Kunimi <takeshi.kunimi@nextbeat.net>
2017-05-15 00:30:38 +0900

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,16 +4,11 @@ import com.github.tototoshi.csv.CSVWriter
 import net.ruippeixotog.scalascraper.browser.{ Browser, JsoupBrowser }
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 
 object Main extends App {
   val browser     = JsoupBrowser()
-  val loginForm   = Map(
-    "stat" -> "login",
-    "id" -> "hplus333",
-    "pw" -> "62423225"
-  )
-  val loginResult = browser.post("http://www.club-y.info/yindex.php", loginForm)
+
+  login(browser)
 
   val f = new File("out.csv")
   val writer = CSVWriter.open(f)
@@ -21,15 +16,24 @@ object Main extends App {
   writer.writeRow(header)
 
   val makers = getMakers(browser)
+
   makers.foreach { maker =>
-    write(browser, writer, maker, 0)
+    write(browser, writer, maker)
   }
 
   writer.close()
 
 
-
   // ------- functions -------
+  def login(browser: Browser): Unit = {
+    val loginForm = Map(
+      "stat" -> "login",
+      "id" -> "hplus333",
+      "pw" -> "62423225"
+    )
+    browser.post("http://www.club-y.info/yindex.php", loginForm)
+  }
+
   /**
    * メーカーの一覧の取得
    */
@@ -42,7 +46,7 @@ object Main extends App {
   /**
    * 渡されたurlの商品を渡されたwriterで書き込む。
    */
-  def write(browser: Browser, writer: CSVWriter, maker: String, page: Int): Unit = {
+  def write(browser: Browser, writer: CSVWriter, maker: String, page: Int = 0): Unit = {
     val url       = s"http://www.club-y.info/salon-norder1.php?vmcode=3067&stat=search&sgoodsname=&smakername=$maker&sseriesname=&scat=&pagenum=$page&ordercode=&getcode=&idcode="
     val doc       = browser.get(url)
     val goodsList = (doc >> elementList("#HPB_TABLE_2_B_100212205608 tr")).drop(3).dropRight(3)
